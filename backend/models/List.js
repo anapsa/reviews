2b98fea274e3c065f678bdf23fab2d711f30f0a4
xupenio04 <pedrocampello17@gmail.com>
2025-02-09 14:38:21 -0300

    feat(models): creating member and list struture for the database

---
+++ backend/models/List.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const MemberSchema = new mongoose.Schema({
+  id: { type: String, required: true },  
+  name: { type: String, required: true },
+  gender: { type: String, required: true },
+  description: { type: String, required: true },
+  userAvaliation: { type: Number, required: true }
+});
+
+const ListSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  type: { type: String, required: true, enum: ["watched", "abandoned"] },  
+  members: { type: [MemberSchema], default: [] }
+}, { timestamps: true });
+
+const List = mongoose.model("List", ListSchema);
+module.exports = List;
