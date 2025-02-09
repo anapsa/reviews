d23b1ef12e449cd50bbdaeed49f8a1b1cfb78138
xupenio04 <pedrocampello17@gmail.com>
2025-02-09 13:58:46 -0300

    fix: removing some files that i created before

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
 import app from './app';
 import logger from './logger';
 import Env from './env';
+import { Express, request, response } from 'express';
 
 app.listen(Env.PORT, () => {
   logger.info(`Server started on http://localhost:${Env.PORT}/api`);
 });
+
+app.get('/ping', (request,response) =>{
+    response.json('Temos um GET')
+});
+
+
