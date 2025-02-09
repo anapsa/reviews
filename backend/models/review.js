37720331dcebb59c8cefebeafc6742cefc05c379
Ana Paula Sá <anapaulasapaiva@gmail.com>
2025-02-09 20:11:04 -0300

    feat: create review mongodb schema, review controller and login

---
+++ backend/models/review.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: false}, 
+  classification: { type: Number, required: true}, 
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: mongoose.Schema.Types.ObjectId, ref: "Content", required: true }
+}, { timestamps: true });
+
+
+const Review = mongoose.model("Review", ReviewSchema);
+module.exports = Review;
