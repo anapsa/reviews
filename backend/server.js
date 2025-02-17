65e66c1224c4c0ec79870d61698987e78068c0b0
LightAsh04 <lsa4@cin.ufpe.br>
2025-02-16 22:57:52 -0300

    merge: solving conflicts for integration with movie CRUD

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./database/db");
 const userRoutes = require("./routes/userRoutes");
-<<<<<<< HEAD
 const listRoutes = require('./routes/listRoutes');
 const cors = require("cors");
 const reviewRoutes = require("./routes/reviewRoutes");
 const commentRoutes = require("./routes/commentRoutes");
-=======
 const movieRoutes = require("./routes/movieRoutes")
->>>>>>> crudMovie
 
 const app = express();
 
@@ -22,14 +19,11 @@ app.use(express.json());
 
 // Rota para efetuar procedimentos com os usuários do banco
 app.use("/users", userRoutes);
-<<<<<<< HEAD
 app.use("/api",listRoutes)
 app.use("/reviews", reviewRoutes);
 app.use("/comment", commentRoutes)
-=======
 //Rota para efetuar procedimentos com os filmes do banco
 app.use("/movies", movieRoutes)
->>>>>>> crudMovie
 // Inicia o servidor
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
