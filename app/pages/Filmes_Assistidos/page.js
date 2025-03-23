e98381c608cc4b4bd156f8cffd46d6ce31ce2910
xupenio04 <pedrocampello17@gmail.com>
2025-03-23 19:04:32 -0300

    fix: my page commit

---
+++ app/pages/Filmes_Assistidos/page.js
@@ -0,0 +1,78 @@
+'use client';
+import Sidebar from '../../components/sidebar/sidebar';
+import SearchBar from '../../components/searchbar/searchbar';
+import MovieInfo from '../../components/filme_info/movie_info';
+import TopBar from '../../components/topbar/topbar';
+import PopUpAdicao from '../../components/popUp_adicao/popUp_adicao';
+import PopUpRemocao from '../../components/popUp_remocao/popUp_remocao';
+import { use, useEffect, useState } from 'react';
+import './style.css';
+
+export default function Home() {
+    const [IsAddButton, SetIsAddButton] = useState(false);
+    const [IsRemoveButton, SetIsRemoveButton] = useState(false);
+    const [IsWatched, SetIsWatched] = useState(true); // Inicia com filmes assistidos
+    const [IsAbandoned, SetIsAbandoned] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const [IsSearch, SetIsSearch] = useState(false);
+
+    // Simulando a busca de filmes no backend
+    useEffect(() => {
+        async function fetchMovies() {
+            const user = "xupenio";
+
+            const watchedResponse = await fetch(`http://localhost:5001/users/${user}/watched`);
+            const abandonedResponse = await fetch(`http://localhost:5001/users/${user}/abandoned`);
+
+            const watchedMovies = await watchedResponse.json();
+            const abandonedMovies = await abandonedResponse.json();
+
+            setMovies([
+                ...watchedMovies.map(m => ({ ...m, type: "watched" })),
+                ...abandonedMovies.map(m => ({ ...m, type: "abandoned" }))
+            ]);
+
+           
+            setFilteredMovies(watchedMovies.map(m => ({ ...m, type: "watched" })));
+        }
+
+        fetchMovies();
+    }, []);
+
+    useEffect(() => {
+        if (IsWatched) {
+            setFilteredMovies(movies.filter(movie => movie.type === "watched"));
+        } else if (IsAbandoned) {
+            setFilteredMovies(movies.filter(movie => movie.type === "abandoned"));
+        }
+
+    }, [IsWatched, IsAbandoned, movies]);
+    return (
+        <div id='home'>
+            <TopBar
+                SetIsAddButton={SetIsAddButton}
+                SetIsRemoveButton={SetIsRemoveButton}
+                SetIsWatched={SetIsWatched}
+                SetIsAbandoned= {SetIsAbandoned}
+            />
+
+            {IsAddButton && (
+                <div className='container'>
+                    <PopUpAdicao SetIsAddButton={SetIsAddButton} />
+                </div>
+            )}
+
+            {IsRemoveButton && (
+                <div className='container'>
+                    <PopUpRemocao SetIsRemoveButton={SetIsRemoveButton} />
+                </div>
+            )}
+
+            <MovieInfo IsWatched={IsWatched} IsAbandoned={IsAbandoned}
+                movies={filteredMovies}
+                listType={IsWatched ? 'watched' : 'abandoned'}
+            />
+        </div>
+    );
+}
